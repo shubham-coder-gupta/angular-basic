de1f0c51a7bd001884d76921d5e0816d04e36c9d
mayanknicobar <mayanksoni@nicobar.com>
2019-01-05 17:54:27 +0530

    basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basic';
+  title = 'angular-basic by shubham';
 }
